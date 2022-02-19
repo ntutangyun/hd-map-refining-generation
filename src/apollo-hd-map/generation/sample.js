1096d5c5798868adb332cff882516d50984bc45f
Tang Yun <ntutangyun@gmail.com>
2022-02-19 16:49:53 +0800

    update

--- src/apollo-hd-map/generation/sample.js
+++ src/apollo-hd-map/generation/sample.js
@@ -1,26 +1,6 @@
 require("../common/setup");
 const fs = require("fs");
 
-// const mapName = "san_francisco";
-//
-// // read existing hd map data
-// const graphData = require(`../../../data/apollo/${mapName}_routing_map.json`);
-// const mapData = require(`../../../data/apollo/${mapName}_base_map.json`);
-//
-// // parse hd map data
-// const Graph = require("../common/ApolloHDMap/Graph");
-// const graph = new Graph(mapName);
-// graph.init(graphData, mapData);
-//
-// // extract junction feature vectors
-// const JunctionFeatureExtractor = require("./FeatureEngineering/JunctionFeatureExtractor");
-//
-// // cluster junction based on their topology and geometry feature
-// const junctionClusters = JunctionFeatureExtractor.junctionTopoGeoClustering(graph);
-//
-// const {buildGridLayout} = require("./FeatureEngineering/GridLayout/JunctionLayoutGrid");
-// const junctionGrid = buildGridLayout(graph, junctionClusters);
-
 const {JunctionGrid} = require("./FeatureEngineering/GridLayout/JunctionGrid");
 const MapGeneratorGrid = require("./Generators/MapGeneratorGrid");
 const {getDirectionAngle} = require("./FeatureEngineering/GridLayout/JunctionGridUtils");
