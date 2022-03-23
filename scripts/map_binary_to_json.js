0421ede13d46e88e6d6fd9aeea367cea855bec6a
Tang Yun <ntutangyun@gmail.com>
2022-03-23 21:32:05 +0800

    update

--- scripts/map_binary_to_json.js
+++ scripts/map_binary_to_json.js
@@ -1,10 +1,14 @@
-const mapBinaryPath = "../data/apollo/san_francisco_base_map.bin";
-const routingBinaryPath = "../data/apollo/san_francisco_routing_map.bin";
-
-const MapProto = require("../src/apollo-hd-map/protobuf_out/modules/map/proto/map_pb");
-const RoutingProto = require("../src/apollo-hd-map/protobuf_out/modules/routing/proto/topo_graph_pb");
-
 const fs = require("fs");
+const path = require("path");
+const mapBinaryPath = "/home/tangyun/Desktop/apollo/modules/map/data/highway101GLE/base_map.bin";
+const routingBinaryPath = "/home/tangyun/Desktop/apollo/modules/map/data/highway101GLE/routing_map.bin";
+
+let config = require("../src/apollo-hd-map/lib/global.config.json");
+global.ApolloTestingLib = (localPath) => require(path.join(config.ApolloTestingLibPath, localPath));
+global.ApolloTestingLib("common/setup");
+
+const MapProto = global.ApolloTestingLib("protobuf_out/modules/map/proto/map_pb");
+const RoutingProto = global.ApolloTestingLib("protobuf_out/modules/routing/proto/topo_graph_pb");
 
 const mapData = fs.readFileSync(mapBinaryPath);
 const map = MapProto.Map.deserializeBinary(mapData);
